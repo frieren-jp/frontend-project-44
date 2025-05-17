0a6a3acc533221c0d7594286d90b8c6c2d83c13f
frieren-jp <hornet1311@gmail.com>
2025-05-17 13:34:46 +0300

    fifth commit Sonar Qube err

--- src/games/even.js
+++ src/games/even.js
@@ -11,8 +11,7 @@ const runEvenGames = () => {
   const roundCount = 3;
 
   for (let i = 0; i < roundCount; i += 1) {
-    const val = Math.random();
-    const number = Math.floor(val * 100);
+    const number = Math.floor(Math.random() * 100);
     const correctAnswer = isEven(number) ? 'yes' : 'no';
 
     console.log(`Question: ${number}`);
