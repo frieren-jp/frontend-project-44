5c6dcdbaef3dbd73ec34010932a17fc40dfb4442
frieren-jp <hornet1311@gmail.com>
2025-05-18 18:47:33 +0300

    fix hexlet err 4.0

--- src/games/even.js
+++ src/games/even.js
@@ -25,7 +25,7 @@ const runEvenGames = () => {
 
     if (userAnswer === correctAnswer) {
       console.log('Correct!')
-    } 
+    }
     else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`)
       console.log(`Let's try again, ${name}!`)
